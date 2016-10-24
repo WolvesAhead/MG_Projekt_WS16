114e80b95b1a37ae104f9cb29f0779650cd8a18f
Eldinox <waldemar.g94@web.de>
2016-10-24 21:15:24 +0200

    erster Prototyp

---
+++ Prototyp_v1.0/Pong 3D/Assets/Player1Control.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Control : MonoBehaviour 
+{
+	public GameObject playerPaddle;
+	private float speed = 6f;
+
+	void Start() 
+	{
+	
+	}
+	
+	void Update() 
+	{
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+	}
+}
---
+++ Prototyp_v1.0/Pong 3D/Assets/Player2Control.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Control : MonoBehaviour 
+{
+	public GameObject playerPaddle;
+	private float speed = 6f;
+
+	void Start()
+	{
+	
+	}
+	
+	void Update() 
+	{
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+	}
+}

e2055cc721feb53ba328ce0129ff2ade05a8d77b
Eldinox <waldemar.g94@web.de>
2016-11-13 12:36:33 +0100

    Mainmenu und Discolevel Rohling hinzugefügt

---
+++ Prototyp_v1.1/Pong3D/Assets/Scripts/Level2Start.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level2Start : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+	
+	}
+
+	public void onClick()
+	{
+		Application.LoadLevel("Level2(Disco)");
+	}
+}
